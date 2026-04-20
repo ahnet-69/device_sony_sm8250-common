6a2bf6e528afa23037e0a8921370159b6d6cbc40
realahnet <syedmusicrealise@gmail.com>
2026-04-20 21:40:22 +0500

    Revert "sm8250-common: Add SKU manifests for single-SIM and dual-SIM devices"
    This reverts commit 51928fb4cb4200fd755fbb7d020d114ce657d956.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,10 +135,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
 
-ODM_MANIFEST_SKUS := ss ds
-ODM_MANIFEST_SS_FILES := $(COMMON_PATH)/configs/vintf/manifest_ss.xml
-ODM_MANIFEST_DS_FILES := $(COMMON_PATH)/configs/vintf/manifest_ds.xml
-
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
