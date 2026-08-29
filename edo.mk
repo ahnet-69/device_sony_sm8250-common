ac34b88815dd878429fc97fc75bd4a88103e3786
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 10:19:42 +0300

    sm8250-common: Drop UFFD flag
    See: Ibb2c5e9191744f6e15f6bced337ba7d0ed9681ca
    Change-Id: I40424ec785b2f1674a5eefe2150bda5c0a170e61

--- edo.mk
+++ edo.mk
@@ -283,9 +283,6 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
-# Kernel
-PRODUCT_ENABLE_UFFD_GC := true
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
